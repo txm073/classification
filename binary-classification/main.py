1fe5602e3081c34e94fa0810661c8a83e555b7fc
Tom Barnes <tomtombarnes2@gmail.com>
2022-01-09 01:17:52 +0000

    Files updated at 09/01/2022 - 01:17

--- binary-classification/main.py
+++ binary-classification/main.py
@@ -1,14 +1,18 @@
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
-from torch import nn, optim
-from torch.utils.data import Dataset, DataLoader
-from sklearn.model_selection import train_test_split
 import os, sys, time, random
+
+import torch
+from torch import nn, optim
+from torch.nn import functional as func
 
 
 DATASET = os.path.expandvars("C:/Users/$USERNAME/Datasets/DogsVsCats")
 IMAGE_SIZE = 50
+BATCH_SIZE = 16
+EPOCHS = 1
+LEARNING_RATE = 1e-3
 
 def preprocess(dataset_path, mode="train"):
     base_path = os.path.dirname(dataset_path) 
@@ -23,6 +27,7 @@ def preprocess(dataset_path, mode="train"):
 def load_batches(dataset_path, batch_size=16, mode="train"):
     path = os.path.join(dataset_path, f"arrays-{mode}")
     listdir = os.listdir(path)
+    load_batches.length = len(listdir)
     random.shuffle(listdir)
     for i in range(0, len(listdir), batch_size):
         file_batch = listdir[i:i+batch_size]
@@ -33,8 +38,58 @@ def load_batches(dataset_path, batch_size=16, mode="train"):
             data = np.load(os.path.join(path, file))
             x_batch.append(data["x"][0])
             y_batch.append(data["y"])
-        yield np.array(x_batch), np.array(y_batch)
+        yield (
+            torch.tensor(np.array(x_batch), dtype=torch.float32), 
+            torch.tensor(np.array(y_batch), dtype=torch.float32)
+        )
 
-train_loader = load_batches(DATASET, batch_size=16)
-x, y = next(train_loader)
 
+class Model(nn.Module):
+
+    def __init__(self, input_shape):
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(input_shape, 1024)
+        self.fc2 = nn.Linear(1024, 256)
+        self.fc3 = nn.Linear(256, 64)
+        self.output_layer = nn.Linear(64, 1)
+        for layer in [self.fc1, self.fc2, self.fc3, self.output_layer]:
+            nn.init.xavier_uniform_(layer.weight)
+            nn.init.zeros_(layer.bias)
+
+    def forward(self, x):    
+        x = func.relu(self.fc1(x))
+        x = func.relu(self.fc2(x))
+        x = func.relu(self.fc3(x))
+        x = torch.sigmoid(self.output_layer(x))
+        return x
+
+
+train_loader = load_batches(DATASET, batch_size=BATCH_SIZE, mode="train")
+test_loader = load_batches(DATASET, batch_size=BATCH_SIZE, mode="test")
+model = Model(input_shape=IMAGE_SIZE * IMAGE_SIZE)
+optimiser = optim.SGD(model.parameters(), lr=LEARNING_RATE)
+loss_fn = nn.BCELoss()
+
+def train():
+    model.train()
+    for i in range(EPOCHS):
+        start = time.time()
+        print(f"Starting epoch {i}...")
+        j = 0
+        while True:
+            j += 1
+            try:
+                x_batch, y_batch = next(train_loader)
+                output = model(x_batch)
+                loss = loss_fn(output.reshape(BATCH_SIZE), y_batch)
+                optimiser.zero_grad()
+                loss.backward()
+                optimiser.step()
+                if j % 100 == 0:
+                    print(f"Completed batch {j}")
+            except StopIteration:
+                break
+        print(f"Completed epoch {i} in {time.time() - start}\n")
+    torch.save(model.state_dict(), "model.pt")
+
+train()
